eb07cc05f6c4c03ba52d27277504664c2258316e
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-10-15 16:18:38 -0500

    RDS Instance Creation
    Need to meet to get everyone situated

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,18 +1,30 @@
 -- create database db
-CREATE DATABASE db;
+CREATE DATABASE museumtours;
 
 -- use newly create database
-USE db;
+USE museumtours;
 
 -- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+CREATE TABLE test_table (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id)
 );
 
 -- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+INSERT INTO test_table (id) VALUES (5);
+
+-- select sample entry
+SELECT * FROM test_table;
+
+-- -- create table in db
+-- CREATE TABLE `db`.`test_table` (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `value` VARCHAR(45),
+--     PRIMARY KEY (`id`),
+--     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+-- );
+
+-- -- insert sample entry
+-- INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
 
 
