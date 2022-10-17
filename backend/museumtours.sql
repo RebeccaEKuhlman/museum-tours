65be0d3e9c5856630fcf33af37e50cf73c7c6424
agenato <agenato@smu.edu>
2022-10-17 15:55:01 -0500

    Fixed table attributes in museumtours.sql

--- backend/museumtours.sql
+++ backend/museumtours.sql
@@ -3,55 +3,55 @@
 USE museumtours;
 
 CREATE TABLE photos(
-	photoId int PRIMARY KEY AUTO_INCREMENT,
+	photoId INT PRIMARY KEY AUTO_INCREMENT,
   	photo_data VARCHAR(100),
-	is_profile bool DEFAULT False
+	is_profile BOOL DEFAULT False
 );
 
 CREATE TABLE Users(
 	username VARCHAR(30) NOT NULL PRIMARY KEY,
 	password VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
-	joinDate Date,
+	joinDate DATE NOT NULL,
 	photoId INT NOT NULL,
-	FOREIGN KEY (photoId) REFERENCES photos(photoId),
   	uni_affilation VARCHAR(50),
-  	is_director bool DEFAULT False,
-  	bio VARCHAR(100)
+  	is_director BOOL DEFAULT False,
+  	bio VARCHAR(100),
+  	FOREIGN KEY (photoId) REFERENCES photos(photoId)
 );
 
 CREATE TABLE ratings(
-	rating int NOT NULL,
+	rating INT NOT NULL,
 	username VARCHAR(30) NOT NULL,
 	tour_Name VARCHAR(30) NOT NULL,
 	theme VARCHAR(30) NOT NULL,
 	museum_name VARCHAR(30) NOT NULL,
 	FOREIGN KEY (username) REFERENCES Users(username),
-  	FOREIGN KEY (tour_Name) REFERENCES Museums(tour_Name),
+  	FOREIGN KEY (tour_Name) REFERENCES Tours(tour_Name),
 	content VARCHAR(100),
 	FOREIGN KEY (theme) REFERENCES Tours(theme),
 	FOREIGN KEY (museum_name) REFERENCES Museums(museum_name),
-	ratingNum int NOT NULL AUTO_INCREMENT,
+	ratingNum INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (tour_name, ratingNum)
 );
 
 CREATE TABLE comments(
 	commNum int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	content VARCHAR(100) NOT NULL AUTO_INCREMENT,
+	content VARCHAR(100) NOT NULL,
 	username VARCHAR(30) NOT NULL,
 	tour_Name VARCHAR(30) NOT NULL,
 	review_id INT NOT NULL,
 	FOREIGN KEY (username) REFERENCES Users(username),
-   	FOREIGN KEY (tour_Name) REFERENCES Museums(tour_Name),
+   	FOREIGN KEY (tour_Name) REFERENCES Tours(tour_Name),
    	like_sum int DEFAULT 0,
-   	overComment int,
+   	overComment int NOT NULL,
 	FOREIGN KEY(review_id) REFERENCES ratings(ratingNum)
 );
 
 CREATE TABLE Museums(
 	museum_name varchar(30) NOT NULL PRIMARY KEY,
 	photoId INT NOT NULL,
-	username VARCHAR(30) NOT NULL,
+	director VARCHAR(30) NOT NULL,
 	FOREIGN KEY (director) REFERENCES Users(username),
 	num_exhibits int NOT NULL DEFAULT 0,
 	FOREIGN KEY (photoId) REFERENCES photos(photoId)
@@ -61,11 +61,11 @@ CREATE TABLE Tours(
 	tour_Name VARCHAR(30) NOT NULL PRIMARY KEY,
 	tourDate Date NOT NULL,
 	tourTime Time NOT NULL,
-	num_spaces_availble int NOT NULL,
+	num_spaces_available int NOT NULL,
 	total_space int NOT NULL,
 	tour_description VARCHAR(30),
 	price int,
 	museum_name varchar(30) NOT NULL,
-	FOREIGN KEY (museum) REFERENCES Museums(museum_name),
+	FOREIGN KEY (museum_name) REFERENCES Museums(museum_name),
 	theme VARCHAR(30) NOT NULL
 );
