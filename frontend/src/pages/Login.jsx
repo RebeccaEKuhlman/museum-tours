a45f32a1c663a9988cd583b94a2e8306a9e6f1d2
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-10-19 19:02:54 -0500

    Final Changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Card, CardActions, CardContent, CardMedia } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import axios from "axios";
 import "./Login.css";
 import { Repository } from "./repository";
