bb7c50bae4264696ce12f8f8e1a41c15f389dbe7
blake-gebhardt <blakewgebhardt@gmail.com>
2022-10-25 11:30:37 -0500

    Stopped styling so I can go work on other pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,307 +1,157 @@
-import React, { useEffect, useState } from "react";
-import { Typography } from "@material-ui/core";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import axios from "axios";
-import "./Login.css";
-import { Repository } from "./repository";
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-// React functional component
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Version "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    fontFamily: "Baskerville",
+  },
+  palette: {
+    background: {
+      paper: "#F6F7EB",
+    },
+    text: {
+      primary: "#323031",
+      secondary: "#696963",
+    },
+    action: {
+      active: "#001E3C",
+    },
+    success: {
+      dark: "#7F96FF",
+    },
+  },
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage:
+      "url(https://texashighways.com/wp-content/uploads/2021/11/drive-ticket-alamosaurus.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export function Login() {
-  // state for storage of the information on the webpage of forms and list, uses hooks
-  const [number, setNumber] = useState("");
-  const [values, setValues] = useState([]);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = "";
-  // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
-  // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-  const url = ec2 ? ec2_url : "localhost";
-
-  var repository = new Repository();
-  // handle input field state change
-  // const handleChange = (e) => {
-  //   setNumber(e.target.value);
-  // };
-
-  // const fetchBase = () => {
-  //   axios.get(`http://${url}:8000/`).then((res) => {
-  //     alert(res.data);
-  //   });
-  // };
-
-  // fetches vals of db via GET request
-  // const fetchVals = () => {
-  //   axios
-  //     .get(`http://${url}:8000/values`)
-  //     .then((res) => {
-  //       const values = res.data.data;
-  //       console.log(values);
-  //       setValues(values);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
-  // handle input form submission to backend via POST request
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    repository.getLogin(username, password).then((x) => {
-      if (typeof x.data != "undefined") {
-        alert("Logged in");
-      } else if (x.error) {
-        alert("Invalid Credentials");
-      }
-    });
-    // axios
-    // .get(`http://${url}:8000/login`, {
-    //   data: {
-    //     username: username,
-    //     password: password
-    //   }
-    // })
-    // .then((res) => {
-    //   console.log("res");
-    //   console.log(res);
-    // })
-    // .catch((err) => {
-    //   console.log("logging error");
-    //   console.log(err);
-    // });
-    // Do Something With Result (Route to New Location)
-  };
-
-  // handle intialization and setup of database table, can reinitialize to wipe db
-  // const reset = () => {
-  //   axios
-  //     .post(`http://${url}:8000/reset`)
-  //     .then((res) => {
-  //       console.log(res);
-  //       fetchVals();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
-
-  // tell app to fetch values from db on first load (if initialized)
-  // the comment below silences an error that doesn't matter.=
-  // useEffect(() => {
-  //   fetchVals();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const classes = useStyles();
 
   return (
-    <div className="Front-div">
-      <header className="Front-header">
-        <nav>
-          <p>Login</p>
-        </nav>
-      </header>
-      <body className="Front-body">
-        {/* <section id="homeView">
-          <div>
-            <section id="carousel">
-              <Card
-                sx={{
-                  maxWidth: 400,
-                  margin: "auto",
-                  transition: "0.3s",
-                  m: 5,
-                  p: 5,
-                  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-                  "&:hover": {
-                    boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
-                  },
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  alt="dma"
-                  height="200"
-                  image="https://www.dma.org/sites/default/files/dma-logo.png"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Dallas Museum of Art
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Art museum with more than 24,000 works of art from around
-                    the world ranging from ancient to modern times. Located in
-                    downtown Dallas.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-              <Card
-                sx={{
-                  maxWidth: 400,
-                  margin: "auto",
-                  transition: "0.3s",
-                  m: 5,
-                  p: 5,
-                  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-                  "&:hover": {
-                    boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
-                  },
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  alt="perot"
-                  height="200"
-                  image="http://photos.prnewswire.com/prnfull/20120531/DC16400LOGO"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Perot Museum of Natural Science
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Dallas's natural history museum. Focuses on innovation,
-                    archaeology, and curiosity. Located in downtown Dallas.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-              <Card
-                sx={{
-                  maxWidth: 400,
-                  margin: "auto",
-                  transition: "0.3s",
-                  m: 5,
-                  p: 5,
-                  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-                  "&:hover": {
-                    boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
-                  },
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  alt="bush_center"
-                  height="200"
-                  image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Official_logo_of_the_George_W._Bush_Presidential_Library.svg/1200px-Official_logo_of_the_George_W._Bush_Presidential_Library.svg.png"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    George Bush Presidential Library
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Presidential Library and think tank of George W. Bush.
-                    Located on the campus of SMU.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-              <Card
-                sx={{
-                  maxWidth: 400,
-                  margin: "auto",
-                  transition: "0.3s",
-                  m: 5,
-                  p: 5,
-                  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
-                  "&:hover": {
-                    boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)",
-                  },
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  alt="meadows_museum"
-                  height="200"
-                  image="https://meadowsmuseumdallas.org/wp-content/uploads/2017/02/MUSE_retina.png"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Meadows Museum
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    The largest collection of Spanish art outside of Spain.
-                    Located on the campus of SMU.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-            </section>
-          </div>
-              </section>*/}
-      </body>
-      <body className="loginBody">
-        <section id="loginView">
-          <h2>LOGIN</h2>
-          <hr class="break" />
-          <form class="form-field required" onSubmit={handleSubmit}>
+    <Grid
+      container
+      component="main"
+      className={classes.root}
+      sx={{ backgroundcolor: "#F6F7EB" }}
+    >
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}></Avatar>
+          <Typography
+            component="h1"
+            className = "title"
+            sx={{
+              fontFamily: "Baskerville",
+            }}
+          >
+            LOGIN
+          </Typography>
+          <form className={classes.form} noValidate>
             <TextField
+              variant="outlined"
+              margin="normal"
               required
-              value={username}
-              onInput={(e) => setUsername(e.target.value)}
-              id="username-required"
+              fullWidth
+              id="username"
               label="Username"
-              defaultValue=""
-              sx={{
-                mb: 2,
-                width: 250,
-                background: "white",
-              }}
+              name="username"
+              autoComplete="username"
+              autoFocus
             />
             <TextField
+              variant="outlined"
+              margin="normal"
               required
-              value={password}
-              onInput={(e) => setPassword(e.target.value)}
-              id="outlined-password-input"
+              fullWidth
+              name="password"
               label="Password"
               type="password"
+              id="password"
               autoComplete="current-password"
-              sx={{
-                mb: 2,
-                width: 250,
-                background: "white",
-              }}
             />
-            <div />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
             <Button
-              variant="submit"
               type="submit"
-              label="Submit"
-              sx={{
-                m: 2,
-                background: "#7F96FF",
-                color: "#F6F7EB",
-                p: 1,
-                width: 250,
-                fontWeight: "bold",
-                "&:hover": { color: "#7F96FF" },
-              }}
+              fullWidth
+              variant="contained"
+              color="##7F96FF"
+              className={classes.submit}
             >
-              {" "}
-              Submit
+              Sign In
             </Button>
-            <h4>New? Register here.</h4>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
           </form>
-        </section>
-      </body>
-    </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
