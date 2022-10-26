dd0d54cb40e63f58b56d7cfeb0ad43443afdd030
zmbenyacoub <73254782+zmbenyacoub@users.noreply.github.com>
2022-10-26 03:08:14 -0500

    styled app nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
+import { sizeHeight } from '@mui/system';
 import React from 'react';
+import { render } from 'react-dom';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 // import { NoPage } from './pages/NoPage';
 
 export const App = () => {
+    const  linkStyle = {
+      color: "white",
+      textDecoration: "none"
+    };
   return (
     <Router>
       <div className="App">
         <ul className="App-header">
           <li>
-            <Link to="/">Home</Link>
+            <Link style={linkStyle} to="/">Home</Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link style={linkStyle} to="/login">Login</Link>
           </li>
         </ul>
         <Routes>
