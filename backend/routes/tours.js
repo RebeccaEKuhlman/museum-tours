4439c3eae013dbc337de70dc029ef8390f1628a4
agenato <agenato@smu.edu>
2022-10-31 21:17:01 -0500

    Fixed GET route for tours.js

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -14,26 +14,27 @@
 const { raw } = require('body-parser');
 const knex = require('../database/knex.js');
 const express = require('express');
+const pool = require('../db');
 
 module.exports = function users(app, logger) {
 
     //fetchToursByName, fetchToursByMuseum_name, and fetchAllTours
+    //getting an error
     app.get('/tours', async (request, response) => {
         try {
-            console.log('Initiating GET /tours request');
             const bodyParser = require('body-parser');
             app.use(bodyParser.json());
 
             if(request.body.tour_Name){
-                const results = await request.models.tour.fetchToursByName(request.body.tour_Name);
+                const results = await knex("tours").select().where( {'tour_Name':request.body.tour_Name} );
                 response.status(201).json(results);
             }
-            else if(request.query.museum_name){
-                const results = await request.models.tour.fetchToursByMuseum_name(request.body.museum_name);
+            else if(request.body.museum_name){
+                const results = await knex("tours").select().where( {'museum_name':request.body.museum_name} );
                 response.status(201).json(results);
             }
             else{
-                const results = await request.models.tour.fetchAllTours();
+                const results = await knex("tours").select();
                 response.status(201).json(results);
             }
         } catch (err) {
@@ -41,55 +42,4 @@ module.exports = function users(app, logger) {
             response.status(500).json({ message: err.message });
         }
     });
-
-    /*app.put('/users/updatePassword', async (request, response) => {
-        try {
-            const username = request.body.username
-            const newPass = request.body.password
-            const { createHash } = require('crypto');
-        function hash(string) {
-            return createHash('sha256').update(string).digest('hex');
-        }
-        const hashed = hash(newPass + 'aB6nkeF0He3imq4AOhbO5kEljbveRpLn');
-        const query = knex('users')
-            .where({ username: username })
-            .update({ password: hashed })
-        const results = await query;
-        console.log('Results of my PUT statement: ', results);
-        response.status(200).json({ username: username});
-        } catch (err) {
-            console.error('There was an error in PUT /users/updatePassword', err);
-            response.status(500).json({ message: err.message });
-        }
-    })
-    app.put('/users/updateBio', async (request, response) => {
-        try {
-            const username = request.body.username
-            const newBio = request.body.bio
-            const query = knex('users')
-                .where({ username: username })
-                .update({ bio: newBio })
-        const results = await query;
-        console.log('Results of my PUT statement: ', results);
-        response.status(200).json({ username: username, bio: newBio});
-        } catch (err) {
-            console.error('There was an error in PUT /users/updateBio', err);
-            response.status(500).json({ message: err.message });
-        }
-    })
-    app.delete('/users', async (request, response) => {
-        try {
-            console.log('Initiating DELETE /users request');
-            console.log('Request has a body / payload containing:', request.body);
-            console.log('Request has params containing:', request.query);
-            const username = request.body.username
-            const query = knex('users').delete().where({username});
-            const results = await query;
-            console.log('Results of my DELETE statement:', results);
-            response.status(200).json(results);
-        } catch (err) {
-            console.error('There was an error in DELETE /users', err);
-            response.status(500).json({ message: err.message });
-        }
-    });*/
 }
