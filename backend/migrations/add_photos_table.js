9f97b8364bf1e37e2f811cd465209d4d5a991818
agenato <agenato@smu.edu>
2022-11-09 19:30:48 -0600

    add_photos_table.js

---
+++ backend/migrations/add_photos_table.js
@@ -0,0 +1,14 @@
+//add_photos_table.js
+exports.up = function(knex) {
+    return knex.schema.createTable('photos', (table) => {
+      table.int('photoId').notNullable();
+      table.bool('is_profile');
+      table.string('photo_data');
+      table.string('caption');
+      table.primary('photoId');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('tours');
+};
--- backend/migrations/add_users_table.js
+++ backend/migrations/add_users_table.js
@@ -1,11 +1,18 @@
 //add_users_table.js
 exports.up = function(knex) {
  return knex.schema.createTable('users', (table) => {
-   table.string('email').notNullable();
+   table.string('username').notNullable();
    table.string('password').notNullable();
-   table.primary('email');
+   table.string('email').notNullable();
+   table.date('tourDate').notNullable();
+   table.int('photoId').notNullable();
+   table.bool('is_director');
+   table.string('bio');
+   table.primary('username');
+   table.foreign('photoId').references('photos.photoId');
  });
 };
+
 exports.down = function(knex) {
  return knex.schema.dropTable('users');
 };
