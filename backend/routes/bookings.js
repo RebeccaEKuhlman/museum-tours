70c3daab8452ffa66f38cf201d073c9b9a79efe5
agenato <agenato@smu.edu>
2022-11-20 17:02:17 -0600

    POST /bookings now return content

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -32,16 +32,24 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        if (req.query.username && req.query.tourName) {
-            const updatedUser = await req.models.booking.insert_booking(req.query.username, req.query.tourName);
-            res.json(updatedUser);
+        if (req.body.username && req.body.tourName) {
+            const updatedUser = await req.models.booking.insert_booking(req.body.username, req.body.tourName);
+
+            if(updatedUser){
+                const check = await req.models.booking.getSpecificBooking(req.body.username, req.body.tourName);
+                res.status(201).json(check); //returns the content
+            }
+            else{
+                console.error('There was an error in POST /bookings');
+                res.status(500).json();
+            }
             next();
         } else {
             throw new Error('Make sure all needed data is encluded\n');
         }
     } catch (err) {
         console.error('There was an error in POST /bookings', err);
-        response.status(500).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }
 });
 
