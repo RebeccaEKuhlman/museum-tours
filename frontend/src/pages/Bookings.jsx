06d9a776e4571ed8104aa5529ae26176ed2a3bd7
blake-gebhardt <blakewgebhardt@gmail.com>
2022-11-25 23:43:25 -0800

    Bookings show up when you click each tour

--- frontend/src/pages/Bookings.jsx
+++ frontend/src/pages/Bookings.jsx
@@ -6,7 +6,7 @@ import {
   CardActions,
   Typography,
   Paper,
-  Grid
+  Grid,
 } from "@material-ui/core";
 import axios from "axios";
 import { Repository } from "../api";
@@ -19,6 +19,8 @@ export const Bookings = () => {
   const params = useParams();
 
   var repository = new Repository();
+
+  var tours = repository.getToursByMuseum(params.museum_name);
 
   // get the context from ScheduleContext
   // const { addToTours } = useContext(ScheduleContext).context;
@@ -35,6 +37,94 @@ export const Bookings = () => {
   //   addToTours(tour);
   //   nav("/profile");
   // };
+
+  function GridItem(props) {
+    let items = [
+      <Paper
+        className="Project"
+        style={{
+          backgroundColor: "ivory",
+          alignItems: "center",
+          marginBottom: 10,
+          display: "flex",
+        }}
+        elevation={10}
+        key={props.item.museum_name}
+      >
+        <CardContent className="Content" style={{ display: "flex" }}>
+          <img
+            className="Image"
+            alt="museumlogo"
+            src="https://1000logos.net/wp-content/uploads/2021/12/Microsoft-Teams-Logo-2016.png"
+            style={{
+              height: 150,
+              width: "auto",
+              borderRadius: "16px",
+            }}
+          ></img>
+          <div className="Info" style={{ marginLeft: 30, width: 500 }}>
+            <Typography
+              className="Title"
+              style={{
+                color: "#696963",
+                fontFamily: "Baskerville",
+                fontWeight: "bold",
+                fontSize: 30,
+              }}
+            >
+              {props.item.tour_Name}
+            </Typography>
+            <hr></hr>
+            <Typography
+              className="Schedule"
+              style={{
+                color: "#696963",
+                fontFamily: "Baskerville",
+                fontSize: 20,
+              }}
+            >
+              {props.item.tourDate} at {props.item.tourTime}
+            </Typography>
+            <Typography
+              className="Description"
+              style={{
+                color: "#696963",
+                fontFamily: "Baskerville",
+                fontSize: 20,
+              }}
+            >
+              {props.item.tourDescription}
+            </Typography>
+            <Typography
+              className="Theme"
+              style={{
+                color: "#696963",
+                fontFamily: "Baskerville",
+                fontSize: 20,
+              }}
+            >
+              {props.item.theme}
+            </Typography>
+            <Typography
+              className="Slots"
+              style={{
+                color: "#696963",
+                fontFamily: "Baskerville",
+                fontSize: 20,
+              }}
+            >
+              Available Space: {props.item.num_spaces_available}
+            </Typography>
+          </div>
+        </CardContent>
+      </Paper>,
+    ];
+    return (
+      <Grid item xs={11}>
+        {items}
+      </Grid>
+    );
+  }
 
   return (
     <div style={{ marginTop: 10, maxWidth: "100%" }}>
@@ -60,7 +150,11 @@ export const Bookings = () => {
           >
             Tours at {params.museum_name}
           </Typography>
-        </Card>
+          </Card>
+        {tour.map((item, index) => {
+          return <GridItem item={item} key={index} />;
+        })}
+         
       </Grid>
     </div>
   );
